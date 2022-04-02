550ae93848144a22d47049111e54540d71426798
Lewis <dsapione@gmail.com>
2022-04-02 19:39:45 -0400

    updated validators, finished models

--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ const UserSchema = new Schema(
 			type: String,
 			required: true,
 			unique: true,
-			// type: Schema.Types.Email,
+			match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
 		},
 		thoughts: [
 			{
